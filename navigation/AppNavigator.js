cfd94f8234c2a6ab91005728cb325dac7a3511b9
MEECHAI Vincent <43035257+PokSoul@users.noreply.github.com>
2024-05-31 14:37:01 +0200

    :white_check_mark: add splash screen :white_check_mark:

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import SplashScreen from '../screens/SplashScreen';
 //import AddExpenseScreen from '../screens/AddExpenseScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       {/* <Stack.Screen name="AddExpense" component={AddExpenseScreen} options={{ title: 'Add Expense' }} /> */}
     </Stack.Navigator>
