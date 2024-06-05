e32fd9ef708fa19cf1a02e6b6b8a9331cb4b8611
MEECHAI Vincent <43035257+PokSoul@users.noreply.github.com>
2024-06-05 13:59:41 +0200

    :white_check_mark: ajout vue addscreen expense :white_check_mark:

--- screens/AddExpenseScreen.js
+++ screens/AddExpenseScreen.js
@@ -1,19 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import styled from 'styled-components/native';
 import Header from '../components/Header';
+import { Feather } from '@expo/vector-icons';
 
-const AddExpenseScreen = ({ toggleTheme, isDarkTheme }) => {
+const AddExpenseScreen = ({ navigation, toggleTheme, isDarkTheme }) => {
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
-    <Container>
-      <Header title="Expense Tracker" toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
-      {/* Ajoutez le formulaire pour ajouter des dépenses ici */}
-    </Container>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <Container>
+        <Header title="Expense Tracker" toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+        <Content>
+          <Title>Your Expense</Title>
+          <InputContainer>
+            <Input
+              placeholder="Amount"
+              placeholderTextColor={(props) => props.theme.placeholder}
+              value={amount}
+              onChangeText={setAmount}
+              keyboardType="numeric"
+            />
+            <Input
+              placeholder="Date"
+              placeholderTextColor={(props) => props.theme.placeholder}
+              value={date}
+              onChangeText={setDate}
+            />
+            <DescriptionInput
+              placeholder="Description"
+              placeholderTextColor={(props) => props.theme.placeholder}
+              value={description}
+              onChangeText={setDescription}
+              multiline
+            />
+          </InputContainer>
+          <ButtonContainer>
+            <CancelButton onPress={() => navigation.goBack()}>
+              <CancelText>Cancel</CancelText>
+            </CancelButton>
+            <AddButton
+              onPress={() => {
+                /* Handle add expense */
+              }}
+            >
+              <AddText>Add</AddText>
+            </AddButton>
+          </ButtonContainer>
+        </Content>
+      </Container>
+    </KeyboardAvoidingView>
   );
 };
 
 const Container = styled.View`
   flex: 1;
   background-color: ${(props) => props.theme.background};
+`;
+
+const Content = styled.View`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  text-align: center;
+  color: ${(props) => props.theme.primary};
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.View`
+  margin-bottom: 20px;
+`;
+
+const Input = styled.TextInput`
+  background-color: ${(props) => props.theme.inputBackground};
+  color: ${(props) => props.theme.text};
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
+
+const DescriptionInput = styled(Input)`
+  height: 80px;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+const CancelButton = styled.TouchableOpacity`
+  flex: 1;
+  margin-right: 10px;
+  background-color: ${(props) => props.theme.cancelButtonBackground};
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const AddButton = styled.TouchableOpacity`
+  flex: 1;
+  margin-left: 10px;
+  background-color: ${(props) => props.theme.addButtonBackground};
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const CancelText = styled.Text`
+  text-align: center;
+  color: ${(props) => props.theme.cancelButtonText};
+  font-weight: bold;
+`;
+
+const AddText = styled.Text`
+  text-align: center;
+  color: ${(props) => props.theme.addButtonText};
+  font-weight: bold;
 `;
 
 export default AddExpenseScreen;
--- styles/themes.js
+++ styles/themes.js
@@ -1,17 +1,23 @@
 export const lightTheme = {
-  background: '#ffffff',
+  background: '#E5E5E5',
   text: '#000000',
+  headerBackground: '#6200ea',
   primary: '#ff6f00', // Orange vif
+  inputBackground: '#FFC599',
+  placeholder: '#a9a9a9',
   secondary: '#ff1744', // Rouge vif
-  buttonBackground: '#ff6f00', // Orange vif
+  addButtonBackground: '#ff6f00', 
   buttonText: '#ffffff',
 };
 
 export const darkTheme = {
-  background: '#121212',
+  background: '#292929',
+  headerBackground: '#1f1f1f',
   text: '#ffffff',
   primary: '#bb86fc', // Violet vif
+  inputBackground: '#EEE1FD',
+  placeholder: '#a9a9a9',
   secondary: '#03dac6', // Cyan vif
-  buttonBackground: '#bb86fc', // Violet vif
+  addButtonBackground: '#bb86fc', 
   buttonText: '#000000',
 };
